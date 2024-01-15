336d7fdb96621d8cfe8229c553a72424e9f82a65
ervinsv92 <ervinsv92@gmail.com>
2024-01-14 18:56:04 -0600

    hasta s5 navegary cargar issue por numero

--- 03-react-query-issues/src/api/githubApi.ts
+++ 03-react-query-issues/src/api/githubApi.ts
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
     baseURL:'https://api.github.com/repos/facebook/react',
-    headers:{}
+    headers:{
+        Authorization:'Bearer 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'
+    }
 })
---
+++ 03-react-query-issues/src/issues/hooks/useIssue.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
+import { Issue } from '../interfaces';
+import { githubApi } from '../../api/githubApi';
+import { sleep } from '../../helpers/sleep';
+
+const getIssueInfo = async (issueNumber:number):Promise<Issue> => {
+    await sleep(2)
+    const {data} = await githubApi.get<Issue>(`/issues/${issueNumber}`)
+    return data;
+}
+
+export const useIssue = (issueNumber:number) => {
+    const issueQuery = useQuery({
+        queryKey: ['issue', issueNumber],
+        queryFn: () => getIssueInfo(issueNumber)
+    });
+}
