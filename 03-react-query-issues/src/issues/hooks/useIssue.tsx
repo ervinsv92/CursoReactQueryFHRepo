f93f01e3be25d2ce3c724ead8f854baa54c4b333
ervinsv92 <ervinsv92@gmail.com>
2024-01-20 17:03:50 -0600

    fin seccion 6

--- 03-react-query-issues/src/issues/hooks/useIssue.tsx
+++ 03-react-query-issues/src/issues/hooks/useIssue.tsx
@@ -2,7 +2,6 @@ import { useQuery } from '@tanstack/react-query'
 import { Issue } from '../interfaces';
 import { githubApi } from '../../api/githubApi';
 import { sleep } from '../../helpers/sleep';
-import { IssueComment } from '../components/IssueComment';
 
 export const getIssueInfo = async (issueNumber:number):Promise<Issue> => {
     await sleep(2)
--- 03-react-query-issues/src/issues/views/ListView.tsx
+++ 03-react-query-issues/src/issues/views/ListView.tsx
@@ -3,12 +3,14 @@ import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
 import { useIssues } from '../hooks';
 import LoadingIcon from '../../shared/components/LoadingIcon';
+import { State } from '../interfaces';
 
 
 export const ListView = () => {
 
+  const [state, setState] = useState<State>();
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
-  const {issuesQuery} = useIssues();
+  const {issuesQuery} = useIssues({state, labels: selectedLabels});
 
   const onLabelChanged = (labelName:string)=>{
     (selectedLabels.includes(labelName))
@@ -23,7 +25,11 @@ export const ListView = () => {
         {
           issuesQuery.isLoading
           ? <LoadingIcon />
-          : <IssueList issues={issuesQuery.data || []}/> 
+          : <IssueList 
+              issues={issuesQuery.data || []}
+              state={state}
+              onStateChanged={(newState) => setState(newState)}
+            /> 
           
         }
         
